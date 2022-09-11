dfc6b89b05b4b137aab325958b7cc65c90bc7183
m-suzuki <rgbkids.g@gmail.com>
2022-09-12 00:53:49 +0900

    Add: e2e test

---
+++ e2e/cypress/e2e/HelloWorld.cy.ts
@@ -0,0 +1,11 @@
+describe('VTeacher Matching Test', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/');
+  })
+
+  it('successfully loads', () => {
+    //cy.visit('http://app.localhost:3000/');
+
+  })
+
+})
